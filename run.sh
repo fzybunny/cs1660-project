466f1cc2a31647748f05fbe431ebefe13d4cdcd5
Jim Tomko <j.tomko.175@gmail.com>
2020-11-22 18:10:14 -0500

    Removes unused helper script.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-#docker run -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix $@
-
-# Run the gui or something.
-docker run -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --user="$(id --user):$(id --group)" who70
